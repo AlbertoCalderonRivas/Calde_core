512921ad960653efba5b427c04075edb67e374e0
AlbertoCalderonRivas <albertocalderonrivas@gmail.com>
2025-04-03 14:42:10 +0200

    prueba bajar calidad fotos

--- js/scroll-text.js
+++ js/scroll-text.js
@@ -3,9 +3,10 @@ const scrollText = document.getElementById("scroll-text");
 document.addEventListener("filteredNodesUpdated", cargarProyectos);
 
 async function cargarProyectos() {
-try {
+    
+    scrollText.innerText = `Loading...`;
+    try {
     let visitCount;
-    let texto = `Loading...`
     // Detectar si estamos en desarrollo local o en producción
     const isLocalhost = window.location.hostname === "127.0.0.1" || window.location.hostname === "localhost";
         
@@ -42,7 +43,7 @@ try {
     }
     
     // 🔹 Texto que se desplazará
-    texto = `⋆౨ৎ˚⟡˖ CREADO x ࣪ALBERTO CALDERÓN RIVAS - PORTFOLIO WEB ──୨ৎ── CONTADOR DE VISITAS [${visitCount}] ──୨ৎ── NÚMERO DE PROYECTOS EN MEMORIA [${totalProyectos}] ──9ৎ── NÚMERO DE PROYECTOS MOSTRADOS [${proyectosMostrados}]` + textoTag + ` ──9ৎ── ÚLTIMA ACTUALIZACIÓN [01/04/2025] ✶⋆.˚ `;
+    const texto = `⋆౨ৎ˚⟡˖ CREADO x ࣪ALBERTO CALDERÓN RIVAS - PORTFOLIO WEB ──୨ৎ── CONTADOR DE VISITAS [${visitCount}] ──୨ৎ── NÚMERO DE PROYECTOS EN MEMORIA [${totalProyectos}] ──9ৎ── NÚMERO DE PROYECTOS MOSTRADOS [${proyectosMostrados}]` + textoTag + ` ──9ৎ── ÚLTIMA ACTUALIZACIÓN [01/04/2025] ✶⋆.˚ `;
     
     scrollText.innerText = texto.repeat(10);
     scrollText.innerText += scrollText.innerText;
